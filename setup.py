d9fe17890ef6675ef5b500b6bdcad3b1859ad260
carmelgafa <you@example.coc:/Users/cgafa/Desktop/ML/type2fuzzy_working/env/Scripts/python.exe>
2019-04-16 09:02:31 +0200

    version 0.1.18 - chenges to setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
 				'type2fuzzy/type1_defuzzification',
 				'type2fuzzy/type_reduction',
 			],
-	version = '0.1.17',
+	version = '0.1.18',
 	license='GNU',
 	description = 'Library for type-2 fuzzy logic research',
 	long_description=long_description,
